a260b2c0a77e01aff821bf9da5f58c4528b4104b
surya.kanoria <surya.kanoria@flipkart.com>
2018-01-05 10:57:35 +0530

    Added deployment-cli

--- deployer/DeploymentConfigGenerator.js
+++ deployer/DeploymentConfigGenerator.js
@@ -1,36 +1,43 @@
 #!/usr/bin/env node
 var fs = require('fs');
 var path = require('path');
-var argv = require('yargs')
-    .string('config')
-    .argv;
-var deploymentConfigFile = fs.readFileSync(argv.config, {encoding: 'utf-8'});
-var deploymentConfig = JSON.parse(deploymentConfigFile);
-var shouldDeploy = false;
-var releaseConfig = {};
-releaseConfig.appConfig = {};
-deploymentConfig.deploymentJob.forEach(function (deploymentJob) {
-    deploymentJob.appVersions.forEach(function (appVersion) {
-        var updateGraphConfig = releaseConfig.appConfig[appVersion];
-        if (updateGraphConfig === undefined) {
-            updateGraphConfig = {};
-            updateGraphConfig.currentUpdateGraph = {};
-        }
-        if (updateGraphConfig.currentUpdateGraph[deploymentJob.screenType]) {
-            throw new Error('Screen type already present for particular updateGraphVersion');
-        } else {
-            console.log("ReactBundles", appVersion, deploymentJob.bundleName);
-            updateGraphConfig.currentUpdateGraph[deploymentJob.bundleName] = {
-                gitBranch: deploymentJob.branchName,
-                gitRepoUrl: deploymentJob.repoUrl,
-                bundlePath: path.join("ReactBundles", appVersion, deploymentJob.bundleName),
-                shouldDeploy: deploymentJob.shouldDeploy
-            };
-        }
-        releaseConfig.appConfig[appVersion] = updateGraphConfig;
-    });
-    shouldDeploy = deploymentJob.shouldDeploy || shouldDeploy;
-    deploymentJob.shouldDeploy = false;
-});
 
-console.log(JSON.stringify(releaseConfig));
+/**
+ * Generates the release config which is used
+ * to generate update patches and keep a log of
+ * deployments
+ * @param deploymentConfig - deployment config given by the user
+ * @param outputPath - base path of output folder
+ * @returns {{releaseConfig}}
+ */
+function deploymentConfigGenerator(deploymentConfig, outputPath) {
+    var shouldDeploy = false;
+    var releaseConfig = {};
+    releaseConfig.appConfig = {};
+    deploymentConfig.deploymentJob.forEach(function (deploymentJob) {
+        deploymentJob.appVersions.forEach(function (appVersion) {
+            var updateGraphConfig = releaseConfig.appConfig[appVersion];
+            if (updateGraphConfig === undefined) {
+                updateGraphConfig = {};
+                updateGraphConfig.currentUpdateGraph = {};
+            }
+            if (updateGraphConfig.currentUpdateGraph[deploymentJob.screenType]) {
+                throw new Error('Screen type already present for particular updateGraphVersion');
+            } else {
+                updateGraphConfig.currentUpdateGraph[deploymentJob.bundleName] = {
+                    gitBranch: deploymentJob.branchName,
+                    gitRepoUrl: deploymentJob.repoUrl,
+                    bundlePath: path.join(outputPath, "ReactBundles", appVersion, deploymentJob.bundleName),
+                    shouldDeploy: deploymentJob.shouldDeploy
+                };
+            }
+            releaseConfig.appConfig[appVersion] = updateGraphConfig;
+        });
+        shouldDeploy = deploymentJob.shouldDeploy || shouldDeploy;
+        deploymentJob.shouldDeploy = false;
+    });
+
+    return releaseConfig;
+}
+
+module.exports = deploymentConfigGenerator;
--- deployer/UpdateGraphGenerator.js
+++ deployer/UpdateGraphGenerator.js
@@ -1,50 +1,109 @@
-var fs = require('fs');
-var md5 = require('md5');
-var THRESHOLD_LENGTH = 80;
-function generateHashes(filePath) {
-    var hashMap = {};
-    var placeHolderStructure = {};
-    placeHolderStructure.fileStructure = [];
-    var placeHolderResponse = generatePlaceHolder(filePath,placeHolderStructure ,hashMap);
-    hashMap = placeHolderResponse.hashMap;
-    placeHolderStructure = placeHolderResponse.placeHolderStructure;
-    var componentList = [];
-    var placeHolderJson = JSON.stringify(placeHolderStructure);
-    var placeHolderKey = md5(placeHolderJson);
-    componentList.push(placeHolderKey);
-    (Object.keys(hashMap)).forEach(function(key) {
-        componentList.push(key);
-    });
-    hashMap[placeHolderKey] = placeHolderJson;
-}
-
-function generatePlaceHolder(filePath, placeHolderStructure, hashMap) {
-    var componentList = readJSFile(filePath);
-    componentList.forEach(function (component) {
-        var componentMetaModel;
-        if (component.length < THRESHOLD_LENGTH) {
-            componentMetaModel = {
-                text: 'text',
-                value: component
-            }
-        } else {
-            componentMetaModel = {
-                text: 'component',
-                value: md5(component)
-            };
-            hashMap[md5(component)] = component;
-        }
-        placeHolderStructure.fileStructure.push(componentMetaModel);
-    });
+var fs = require('fs-extra');
+var componentGenerator = require('./ComponentGenerator');
+var generatedUpdatePatch = {};
+var hashMap = {};
+/**
+ * Generates the update patch for the given release config
+ * @param configFilePath - releaseconfig generated after generation of React Native Bundles
+ * @param existingPatchPath - updateGraphs already present in production
+ * @param updateGraphVersion - version number for the new update graph
+ */
+function generateUpdatePatch(configFilePath, existingPatchPath, updateGraphVersion) {
+    var configJson = fs.readFileSync(configFilePath, { encoding: 'utf-8' });
+    var config = JSON.parse(configJson);
+    if (config.appConfig) {
+        (Object.keys(config.appConfig)).forEach(function (appVersion) {
+            var updateGraphConfig = config.appConfig[appVersion];
+            console.log('Generating update patch for appVersion: ', appVersion);
+            generateUpdateGraph(updateGraphConfig, existingPatchPath, appVersion, updateGraphVersion);
+        })
+    }
     return {
-        placeHolderStructure: placeHolderStructure,
-        hashMap: hashMap
-    };
+        componentMap: hashMap,
+        updatePatch: generatedUpdatePatch
+    }
 }
 
-function readJSFile(filePath) {
-    var jsFile = fs.readFileSync(filePath, {encoding: 'utf-8'});
-    return jsFile.split("\n");
+/**
+ * generates update graph for a given appversion and inserts it
+ * in the generatedUpdatePatch corresponding to the appVersion
+ * @param updateGraphConfig - config for the given appVersion
+ * @param existingPatchPath - updateGraphs already present in production
+ * @param appVersion
+ * @param updateGraphVersion - version number for the new update graph
+ */
+function generateUpdateGraph(updateGraphConfig, existingPatchPath, appVersion, updateGraphVersion) {
+    var updateGraph = getExistingUpdateGraphForAppVersion(appVersion, existingPatchPath);
+    console.log('existing update graph', JSON.stringify(updateGraph));
+    var newUpdateGraph = sanitizeUpdateGraph(updateGraph, Object.keys(updateGraphConfig.currentUpdateGraph));
+    console.log('santized update graph', JSON.stringify(newUpdateGraph));
+    newUpdateGraph = generateUpdateGraphForAppVersion(updateGraphConfig, newUpdateGraph);
+    console.log('generated update graph', JSON.stringify(newUpdateGraph));
+    newUpdateGraph.currentUpdateGraphVersion = updateGraphVersion;
+    generatedUpdatePatch[appVersion] = newUpdateGraph;
 }
 
-module.exports = generateHashes;
+/**
+ *
+ * @param appVersion - appVersion for which update graph is required
+ * @param existingPatchPath - filePath for update patches in production
+ */
+function getExistingUpdateGraphForAppVersion(appVersion, existingPatchPath) {
+    var existingPatch = getExistingUpdatePatch(existingPatchPath);
+    return Object.assign({}, existingPatch[appVersion]);
+}
+
+/**
+ * gets update patches present in production from the filePath
+ * @param existingPatchPath - filePath for update patches in production
+ * @returns {{production update patches}}
+ */
+function getExistingUpdatePatch(existingPatchPath) {
+    var existingPatch = {};
+    if (existingPatchPath && existingPatchPath !== '' && fs.pathExistsSync(existingPatchPath)) {
+        var existingPatchJSON = fs.readFileSync(existingPatchPath, { encoding: 'utf-8' });
+        if (existingPatchJSON !== '') {
+            existingPatch = JSON.stringify(existingPatchJSON);
+        }
+    }
+    return existingPatch;
+}
+
+/**
+ * Removes all bundles in production which are not to be included in the
+ * new update graph
+ * @param updateGraph - existing update graph
+ * @param bundleNames - bundle names to be included in the new update graph
+ * @returns {{}}
+ */
+function sanitizeUpdateGraph(updateGraph, bundleNames) {
+    var sanitizedUpdateGraph = {};
+    if (updateGraph) {
+        bundleNames.forEach(function (bundleName) {
+            if (updateGraph[bundleName]) {
+                sanitizedUpdateGraph[bundleName] = updateGraph[bundleName];
+            }
+        })
+    }
+    return sanitizedUpdateGraph;
+}
+
+/**
+ * generates update graph and components for an app version config and adds components to the componentMap
+ * @param updateGraphConfig - updategraph config for a particular appVersion
+ * @param productionUpdateGraph - updategraph already existing in production
+ * @returns {generated update graph}
+ */
+function generateUpdateGraphForAppVersion(updateGraphConfig, productionUpdateGraph) {
+    (Object.keys(updateGraphConfig.currentUpdateGraph)).forEach(function (bundleName) {
+        var screenConfig = updateGraphConfig.currentUpdateGraph[bundleName];
+        if (screenConfig.shouldDeploy) {
+            var components = componentGenerator(screenConfig.bundlePath);
+            productionUpdateGraph[bundleName] = components.componentList;
+            hashMap = Object.assign({}, hashMap, components.hashMap);
+        }
+    });
+    return productionUpdateGraph;
+}
+
+module.exports = generateUpdatePatch;
